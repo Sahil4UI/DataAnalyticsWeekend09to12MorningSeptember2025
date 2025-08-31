1b517da7786ad3d46237ff9de7cc4878448ccc1a
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2025-08-31 12:10:37 +0530

    Add files via upload

---
+++ Python/Conditional Statements.py
@@ -0,0 +1,9 @@
+x = int(input("Enter No1:"))
+y = int(input("Enter No2:"))
+#find the largest b/w two numbers
+if x>y:
+    print(f"{x} is largest")
+else:
+    print(f"{y} is largest")
+
+#take a number as input and check whether the number is even odd
